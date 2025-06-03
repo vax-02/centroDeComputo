21d468789734e5574e036470c650d0e32e6e92de
vax-02 <javier.xcn.18@gmail.com>
2025-06-02 22:55:22 -0400

    v4

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,16 @@ use App\Http\Controllers\UserController;
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 $router->post('/login', 'UserController@login');
-$router->get('/docente/materias', 'UserController@materias');
-$router->get('/docente/materia', 'UserController@materia');
+$router->get('/materias', 'UserController@materias');//docente
+$router->get('/materia', 'UserController@materia');//docente
 
-$router->get('/docente/materia/estudiantes', 'UserController@estudiantes');
-$router->get('/docente/materia/estudiante', 'UserController@estudiante');
-$router->get('/docente/materia/estudiantesSeleccion', 'UserController@estudiantesSeleccion');
+$router->get('/materia/estudiantes', 'UserController@estudiantes');//docente
+$router->get('/perfil', 'UserController@estudiante');//docente/materia/estudiante
+$router->get('/estudiantes-grupo', 'UserController@estudiantesSeleccion');//materia/estudianteSeleecion
 
 
 $router->get('/estudiantes/registro', 'UserController@registro');
